ccf67f24b5e4a817dda8bb27860b6f38f679a54b
Nick Chen <haojungc@gmail.com>
2023-11-08 21:01:15 -0800

    Fix class missing warning

--- compile.sh
+++ compile.sh
@@ -1,1 +1,3 @@
-javac -classpath .:./lib/hadoop-core-1.2.1.jar:./lib/commons-logging-1.2.jar:./lib/commons-configuration-1.10.jar:target/dependency/* -d . $(find . -type f -name '*.java')
+CLASS_PATH=".:./lib/hadoop-core-1.2.1.jar:./lib/commons-logging-1.2.jar:./lib/org.apache.commons.httpclient.jar:./lib/commons-configuration-1.10.jar:target/dependency/*"
+
+javac -classpath "$CLASS_PATH" -d . $(find . -type f -name '*.java')
--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-rm -rf out*; java -Xmx16g -classpath .:./lib/*:target/dependency/* WordCount data/fulldata out; more out/part*
+ENTRY_POINT="${1:-UnigramWordCount}"
+INPUT_DIR="${2:-in/fulldata}"
+OUTPUT_DIR="${3:-out/fulldata}"
+MAX_MEM_SIZE="${4:-16g}"
+
+CLASS_PATH=".:./lib/*:target/dependency/*"
+
+rm -rf "$OUTPUT_DIR"; java -Xmx"$MAX_MEM_SIZE" -classpath "$CLASS_PATH" "$ENTRY_POINT" "$INPUT_DIR" "$OUTPUT_DIR"; less "$OUTPUT_DIR"/part*
